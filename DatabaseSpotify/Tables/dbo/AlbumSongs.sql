520e6ce7fdd3198ac4fb47683cf614e6785826dc
FedeBoccio <bocellifede@gmail.com>
2025-03-25 14:45:29 +0100

    Finished adding info to data.sql

--- DatabaseSpotify/Tables/dbo/AlbumSongs.sql
+++ DatabaseSpotify/Tables/dbo/AlbumSongs.sql
@@ -2,5 +2,5 @@ create table dbo.AlbumSongs(
   songId int,
   albumId int,
   CONSTRAINT FK_AlbumSongs_Songs_id FOREIGN KEY ([songId]) REFERENCES [dbo].[Songs] ([id]),
-  CONSTRAINT FK_AlbumSongs_Albums_albumId FOREIGN KEY ([albumId]) REFERENCES [dbo].[Albums] ([albumId])
+  CONSTRAINT FK_AlbumSongs_Albums_name FOREIGN KEY ([albumId]) REFERENCES [dbo].[Albums] ([albumId])
 )
--- DatabaseSpotify/_DATA/data.sql
+++ DatabaseSpotify/_DATA/data.sql
@@ -1,7 +1,22 @@
+delete from SongMusicTypes
+delete AlbumSongs
+delete from PlaylistSongs
 delete from Playlists
 delete from Users
 delete from Songs
 delete from Artists
+delete from Albums
+
+DBCC CHECKIDENT ('dbo.Users', RESEED, 0);
+GO
+DBCC CHECKIDENT ('dbo.Playlists', RESEED, 0);
+GO
+DBCC CHECKIDENT ('dbo.Artists', RESEED, 0);
+GO
+DBCC CHECKIDENT ('dbo.Songs', RESEED, 0);
+GO
+DBCC CHECKIDENT ('dbo.Albums', RESEED, 0);
+GO
 
 INSERT INTO Users
 VALUES 
@@ -12,19 +27,41 @@ VALUES
 
 INSERT INTO Artists
 VALUES
-('Gionata', 'Boschetti', 'SferaEbbasta')
+('Gionata', 'Boschetti', 'SferaEbbasta'),
+('Lorenzo', 'Vinciguerra', 'PapaV'),
+('Morad', 'El Horami', 'Morad')
+
+
 
 INSERT INTO Songs
 VALUES
 ('Panette', (SELECT id from Artists where [artname] = 'SferaEbbasta'), '180'),
 ('Ciny', (SELECT id from Artists where [artname] = 'SferaEbbasta'), '200'),
 ('Visiera A Becco', (SELECT id from Artists where [artname] = 'SferaEbbasta'), '220'),
-('Dexter', (SELECT id from Artists where [artname] = 'SferaEbbasta'), '190')
+('Dexter', (SELECT id from Artists where [artname] = 'SferaEbbasta'), '190'),
+
+('Gol', (SELECT id from Artists where [artname] = 'SferaEbbasta'), '180'),
+('Calcolatrici', (SELECT id from Artists where [artname] = 'SferaEbbasta'), '200'),
+('3uphon', (SELECT id from Artists where [artname] = 'SferaEbbasta'), '220'),
+('Milano Bene', (SELECT id from Artists where [artname] = 'SferaEbbasta'), '190'),
+('G63', (SELECT id from Artists where [artname] = 'SferaEbbasta'), '180'),
+('15 Piani', (SELECT id from Artists where [artname] = 'SferaEbbasta'), '200'),
+
+('Mattone', (SELECT id from Artists where [artname] = 'PapaV'), '180'),
+('Gesu Bambino', (SELECT id from Artists where [artname] = 'PapaV'), '200'),
+('Apparecchiato', (SELECT id from Artists where [artname] = 'PapaV'), '220'),
+('Mafia Slime', (SELECT id from Artists where [artname] = 'PapaV'), '190'),
+
+('Pelele', (SELECT id from Artists where [artname] = 'Morad'), '180'),
+('Se Grita', (SELECT id from Artists where [artname] = 'Morad'), '200'),
+('Sigue', (SELECT id from Artists where [artname] = 'Morad'), '220'),
+('Sonar', (SELECT id from Artists where [artname] = 'Morad'), '190')
+
 
 INSERT INTO Playlists 
 VALUES 
 ('444',(SELECT UserId FROM Users WHERE [nickname] = 'boccio')),
-('Tutto SferaEbbasta',(SELECT UserId FROM Users WHERE [nickname] = 'boccio')),
+('TuttoSferaEbbasta',(SELECT UserId FROM Users WHERE [nickname] = 'boccio')),
 ('Sanse il top',(SELECT UserId FROM Users WHERE [nickname] = 'marioBrioches')),
 ('ITIS',(SELECT UserId FROM Users WHERE [nickname] = 'marioBrioches')),
 ('Prima Playlist',(SELECT UserId FROM Users WHERE [nickname] = 'capo')),
@@ -51,10 +88,59 @@ BEGIN
     PRINT 'MusicTypes ha già della roba dentro'
 END
 ---------------------------------------------------------------
+INSERT INTO PlaylistSongs
+VALUES
+((SELECT playlistId from Playlists where [name] = 'TuttoSferaEbbasta'), (SELECT id from Songs where [title] = 'Panette')),
+((SELECT playlistId from Playlists where [name] = 'TuttoSferaEbbasta'), (SELECT id from Songs where [title] = 'Ciny')),
+((SELECT playlistId from Playlists where [name] = 'TuttoSferaEbbasta'), (SELECT id from Songs where [title] = 'Visiera A Becco')),
+((SELECT playlistId from Playlists where [name] = 'TuttoSferaEbbasta'), (SELECT id from Songs where [title] = 'Dexter')),
+
+((SELECT playlistId from Playlists where [name] = 'ITIS'), (SELECT id from Songs where [title] = 'Sigue')),
+((SELECT playlistId from Playlists where [name] = 'ITIS'), (SELECT id from Songs where [title] = 'Pelele')),
+((SELECT playlistId from Playlists where [name] = 'ITIS'), (SELECT id from Songs where [title] = 'Sonar')),
+((SELECT playlistId from Playlists where [name] = 'ITIS'), (SELECT id from Songs where [title] = 'Se Grita'))
+-----------------------------------------------------------------------------------
+
+INSERT INTO Albums
+VALUES
+(6, 'X2VR'),
+(7, 'Trap Fatta Bene'),
+(4, 'Reinsertado')
+-------------------------------------------------------------------
+INSERT INTO AlbumSongs
+VALUES
+((SELECT id from Songs where [title] = 'Gol'), (SELECT albumId from Albums where [name] = 'X2VR')),
+((SELECT id from Songs where [title] = 'Calcolatrici'), (SELECT albumId from Albums where [name] = 'X2VR')),
+((SELECT id from Songs where [title] = '3uphon'), (SELECT albumId from Albums where [name] = 'X2VR')),
+((SELECT id from Songs where [title] = 'Milano Bene'), (SELECT albumId from Albums where [name] = 'X2VR')),
+((SELECT id from Songs where [title] = 'G63'), (SELECT albumId from Albums where [name] = 'X2VR')),
+((SELECT id from Songs where [title] = '15 Piani'), (SELECT albumId from Albums where [name] = 'X2VR'))
+------------------------------------------------------------------------
+INSERT INTO SongMusicTypes
+VALUES
+((SELECT id from MusicTypes where [type] = 'Trap'), (SELECT id from Songs where [title] = 'Gol')),
+((SELECT id from MusicTypes where [type] = 'Trap'), (SELECT id from Songs where [title] = 'Calcolatrici')),
+((SELECT id from MusicTypes where [type] = 'Trap'), (SELECT id from Songs where [title] = '3uphon')),
+((SELECT id from MusicTypes where [type] = 'Trap'), (SELECT id from Songs where [title] = 'Milano Bene')),
+((SELECT id from MusicTypes where [type] = 'Trap'), (SELECT id from Songs where [title] = 'G63')),
+((SELECT id from MusicTypes where [type] = 'Trap'), (SELECT id from Songs where [title] = '15 Piani')),
+((SELECT id from MusicTypes where [type] = 'Trap'), (SELECT id from Songs where [title] = 'Dexter')),
+((SELECT id from MusicTypes where [type] = 'Trap'), (SELECT id from Songs where [title] = 'Panette')),
+((SELECT id from MusicTypes where [type] = 'Trap'), (SELECT id from Songs where [title] = 'Ciny')),
+((SELECT id from MusicTypes where [type] = 'Trap'), (SELECT id from Songs where [title] = 'Visiera A Becco')),
+
+((SELECT id from MusicTypes where [type] = 'Reggaeton'), (SELECT id from Songs where [title] = 'Sigue')),
+((SELECT id from MusicTypes where [type] = 'Reggaeton'), (SELECT id from Songs where [title] = 'Pelele')),
+((SELECT id from MusicTypes where [type] = 'Reggaeton'), (SELECT id from Songs where [title] = 'Se Grita')),
+((SELECT id from MusicTypes where [type] = 'Reggaeton'), (SELECT id from Songs where [title] = 'Sonar'))
+---------------------------------------------------------------------------------------------------------------
 
 SELECT * from Users
 SELECT * from Playlists
-SELECT * from MusicTypes
 SELECT * from PlaylistSongs
 SELECT * from Songs
 SELECT * from Artists
+SELECT * from Albums
+SELECT * from AlbumSongs
+SELECT * from MusicTypes
+SELECT * from SongMusicTypes
