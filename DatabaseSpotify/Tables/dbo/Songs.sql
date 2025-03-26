7c6bed28e9095ca877efa7652e6d42a8f8912493
FedeBoccio <bocellifede@gmail.com>
2025-03-26 16:06:35 +0100

    ++ imgCode

--- DatabaseSpotify/Tables/dbo/Songs.sql
+++ DatabaseSpotify/Tables/dbo/Songs.sql
@@ -3,6 +3,7 @@ create table dbo.Songs(
   title varchar(100),
   artistid int,
   [time] int,
+  imgCode varchar(MAX),
   CONSTRAINT PK_Songs_id PRIMARY KEY (id),
   CONSTRAINT FK_Songs_Artists_id FOREIGN KEY ([artistId]) REFERENCES [dbo].[Artists] ([id])
 )
--- SpotifyFake/Models/Albums.cs
+++ SpotifyFake/Models/Albums.cs
@@ -4,5 +4,7 @@
     {
         public int albumId { get; set; }
         public string name { get; set; }
+        public string imgCode { get; set; }
+
     }
 }
