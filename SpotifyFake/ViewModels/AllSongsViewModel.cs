99c0bf6623756043bb9a86b71dbd88c96938e917
FedeBoccio <bocellifede@gmail.com>
2025-03-28 15:18:26 +0100

    ++ all songs

---
+++ SpotifyFake/ViewModels/AllSongsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SpotifyFake.Models;
+
+namespace SpotifyFake.ViewModels
+{
+    public class AllSongsViewModel
+    {
+        private List<Songs> songs;
+
+        public List<Songs> Songs { get => songs; set => songs = value; }
+
+        public AllSongsViewModel(List<Songs> songs)
+        {
+            Songs = songs;
+        }
+    }
+}
