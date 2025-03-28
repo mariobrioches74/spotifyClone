06c7a45b6d543208d8f3f20d637bd129598a035e
mariobrioches74 <mariobrioches74@gmail.com>
2025-03-28 10:28:32 +0100

    ++home display genre --cartella img temp

--- SpotifyFake/Controllers/HomeController.cs
+++ SpotifyFake/Controllers/HomeController.cs
@@ -48,21 +48,27 @@ namespace SpotifyFake.Controllers
         public IActionResult HomeViewRightMenu()
         {
             List<Albums> albums = new List<Albums>();
-            List<Artists> artists = new List<Artists>();
-            List<Playlists> playlists = new List<Playlists>();
-            List<Users> users = new List<Users>();
             List<AlbumSongs> albumSongs = new List<AlbumSongs>();
+            List<Artists> artists = new List<Artists>();
+            List<MusicTypes> musicTypes = new List<MusicTypes>();
+            List<Playlists> playlists = new List<Playlists>();
+            List<PlaylistSongs> playlistSongs = new List<PlaylistSongs>();
+            List<SongMusicTypes> songMusicTypes = new List<SongMusicTypes>();
             List<Songs> songs = new List<Songs>();
+            List<Users> users = new List<Users>();
 
             DatabaseAccess data = new DatabaseAccess();
             albums = data.GetAlbums();
-            artists = data.GetArtists();
-            playlists = data.GetPlaylists();
-            users = data.GetUsers();
             albumSongs = data.GetAlbumSongs();
-            songs = data.GetSongs(null,null);
+            artists = data.GetArtists();
+            musicTypes = data.GetMusicTypes();
+            playlists = data.GetPlaylists();
+            //playlistSongs = data.GetPlaylistSongs();
+            songMusicTypes = data.GetSongMusicTypes();
+            songs = data.GetSongs(null, null);
+            users = data.GetUsers();
 
-            return View(new HomeViewRightMenuViewModel(albums,artists,playlists,users,albumSongs,songs));
+            return View(new HomeViewRightMenuViewModel(albums, albumSongs, artists, musicTypes, playlists, playlistSongs, songMusicTypes, songs, users));
         }
 
         public IActionResult PlaylistDetails(int playlistId)
